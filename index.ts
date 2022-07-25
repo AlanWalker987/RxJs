586dbc29645c33df3c72f72329b461c39ccbbe91
AlanWalker987 <gr.karthik065@gmail.com>
2022-07-25 18:27:49 +0530

    take-takeuntil-takewhile

--- index.ts
+++ index.ts
@@ -1,9 +1,58 @@
 import './style.css';
 
-import { of, map, Observable } from 'rxjs';
+import { fromEvent, interval, map, of, take } from 'rxjs';
+import { takeUntil, takeWhile } from 'rxjs/operators';
 
-of('World')
-  .pipe(map((name) => `Hello, ${name}!`))
-  .subscribe(console.log);
+const click$ = fromEvent(document, 'click');
+
+/** Take Operator 
+
+//const number$ = of(1, 2, 3, 4, 5);
+
+// number$.pipe(take(3)).subscribe({
+//   next: console.log,
+//   complete: () => console.log('complete'),
+// });
+
+click$
+  .pipe(
+    map((event) => ({
+      x: event.clientX,
+      y: event.clientY,
+    })),
+    take(1)
+  )
+  .subscribe({
+    next: console.log,
+    complete: () => console.log('completed!!'),
+  });
+
+  */
+
+/** TakeWhile Operator
+
+click$
+  .pipe(
+    map((event) => ({
+      x: event.clientX,
+      y: event.clientY,
+    })),
+    takeWhile(({ y }) => y <= 200, true)
+  )
+  .subscribe({
+    next: console.log,
+    complete: () => console.log('Completed'),
+  });
+
+  */
+
+/** TakeUntil Operator */
+
+const counter$ = interval(1000);
+
+counter$.pipe(takeUntil(click$)).subscribe({
+  next: console.log,
+  complete: () => console.log('Complete'),
+});
 
 // Open the console in the bottom right to see results.
